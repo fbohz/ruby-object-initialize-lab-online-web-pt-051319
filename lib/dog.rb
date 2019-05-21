30d939b50f8eca4ab24fed3ada7d9ca9cf5aaf0c
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-05-21 17:51:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog 
-  def initialize(name, breed="Mutt")
+  def initialize(name, breed="Mutt") #    defaults the breed argument to "Mutt" in an instance variable @breed
     @name = name
     @breed = breed
   end
